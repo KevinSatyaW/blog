5f0fc3556e5377ece14394adb999077b7531b0f5
Pin <kevin.satya21@student.smkti.net>
2022-11-24 08:50:26 +0800

    update kamis

---
+++ app/Http/Controllers/KontenController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\models\Konten;
+use Illuminate\Http\Request;
+
+class KontenController extends Controller
+{
+    public function index(Request $request)
+    {
+        # code...
+    }
+    public function create(Request $request)
+    {
+        # code...
+    }
+    public function store(Request $request)
+    {
+        # code...
+    }
+    public function edit(Request $request, Konten $konten)
+    {
+        # code...
+    }
+    public function update(Request $request, Konten $konten)
+    {
+        # code...
+    }
+    public function delete(Request $request, Konten $konten)
+    {
+        # code...
+    }
+}
---
+++ resources/views/CRUD/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+@section('content')
+<div class="max-w-7xl mx-auto bg-gray-200 flex flex-col px-10 min-h-screen gap-10">
+    <h1 class="text-lg text-stone-700 mt-7">Yahaha</h1>
+    <span class="text-stone-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
+    </span>
+    <span class="text-stone-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
+    </span>
+    <span class="text-stone-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
+    </span>
+    <span class="text-stone-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
+    </span>
+    <span class="text-stone-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
+    </span>
+</div>
+@endsection
