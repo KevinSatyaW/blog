df59577d91507229beccb1cca6dab890b4473b3b
Pin <kevin.satya21@student.smkti.net>
2022-11-24 10:05:05 +0800

    update kamis

--- resources/views/CRUD/index.blade.php
+++ resources/views/CRUD/index.blade.php
@@ -1,21 +1,29 @@
 @extends('layout')
 @section('content')
 <div class="max-w-7xl mx-auto bg-gray-200 flex flex-col px-10 min-h-screen gap-10">
-    <h1 class="text-lg text-stone-700 mt-7">Yahaha</h1>
-    <span class="text-stone-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
-    </span>
-    <span class="text-stone-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
-    </span>
-    <span class="text-stone-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
-    </span>
-    <span class="text-stone-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
-    </span>
-    <span class="text-stone-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam amet reprehenderit, in, earum voluptatem delectus eos harum molestias debitis asperiores, odit exercitationem? Eligendi iusto recusandae ducimus veniam. Eaque, maxime alias!
-    </span>
+    <h1 class="text-lg text-stone-700 mt-7">Ini CRUD</h1>
+    <table class="text-stone-600">
+        <tr class="border">
+            <th class="px-4 py-2 border">No</th>
+            <th class="px-4 py-2 border">Judul</th>
+            <th class="px-4 py-2 border">Konten</th>
+            <th class="px-4 py-2 border">Aksi</th>
+        </tr>
+        <tr>
+            @forelse ($konten as $index => $item)
+            <th class="px-4 py2 border">{{$index + 1}}</th>
+            <th class="px-4 py2 border">{{$item->Judul}}</th>
+            <th class="px-4 py2 border">{{$item->konten}}</th>
+            <th class="px-4 py2 border">
+                <div>
+                    <a href="">Edit</a>
+                    <a href="">Delete</a>
+                </div>
+            </th>
+            @empty
+            <th colsppan ="12" class="px-4 py-2 border">Belum Ada Konten</th>
+            @endforelse
+        </tr>
+    </table>
 </div>
 @endsection
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,14 +8,11 @@
 <div class="flex-1">
     {{-- <h1 class="text-2xl text-center py-5 bg-stone-900">My Blog Yahahah Hayuuu</h1> --}}
     <div class="flex -flex-row gap-10 bg-stone-900 py-5 pl-5">
-    <a href="#" class="bg-stone-300 hover:bg-stone-500 px-3 py-2 rounded-md text-neutral-800 font-semibold transform transition duration-300 hover:scale-110 hover:bg-stone-400 ">
-        Halaman 1
+    <a href="/" class="bg-stone-300 hover:bg-stone-500 px-3 py-2 rounded-md text-neutral-800 font-semibold transform transition duration-300 hover:scale-110 hover:bg-stone-400 ">
+        Konten
     </a>
-    <a href="#" class="bg-stone-300 hover:bg-stone-500 px-3 py-2 rounded-md text-neutral-800 font-semibold transform transition duration-300 hover:scale-110 hover:bg-stone-400 ">
-        Halaman 2
-    </a>
-    <a href="#" class="bg-stone-300 hover:bg-stone-500 px-3 py-2 rounded-md text-neutral-800 font-semibold transform transition duration-300 hover:scale-110 hover:bg-stone-400 ">
-        Halaman 3
+    <a href="{{route('crud.index')}}" class="bg-stone-300 hover:bg-stone-500 px-3 py-2 rounded-md text-neutral-800 font-semibold transform transition duration-300 hover:scale-110 hover:bg-stone-400 ">
+        CRUD
     </a>
     </div>
 </div>
